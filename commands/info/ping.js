61657c95ad2c6479488adc53a1674e359a1c45d9
Salvage_Dev <50248166+Milo123459@users.noreply.github.com>
2020-03-06 07:16:33 +0000

    Initial commit

---
+++ commands/info/ping.js
@@ -0,0 +1,15 @@
+const {MessageEmbed} = require('discord.js');
+module.exports={
+    name: 'ping',
+    category: 'info',
+    description: 'Returns latency and API ping',
+    timeout: 10000,
+    run: async(bot,message,args)=>{
+        const msg = await message.channel.send('🏓 Pinging...')
+        const Embed = new MessageEmbed()
+        .setTitle('🏓Pong!🏓')
+    .setDescription(`🏓🏓🏓\nLatency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}MS\nAPI Latency is ${Math.round(bot.ws.ping)}MS\n🏓🏓🏓`)
+    .setColor('RANDOM')
+    msg.edit(Embed)
+    }
+}
